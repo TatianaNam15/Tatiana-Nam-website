c2785cbfe9c05f607f714ccb75dd87f67904fbd3
Tatiana Nam <74437476+TatianaNam15@users.noreply.github.com>
2022-06-01 22:15:13 -0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,3 +1,7 @@
+gsap.from('#name', {delay: .5, duration: 3, scale: 0, ease: "elastic"});
+
+gsap.from('.nav-btn', {delay: .5, duration: 3.5, rotation: 720});
+
 // Navigation
 
 const burger = document.querySelector('.nav-btn');
@@ -8,52 +12,93 @@ burger.addEventListener('click', ()=> {
     navList.classList.toggle('show');
 })
 
-// Projects
-
-const project = document.querySelectorAll('.project');
-console.log(project);
-
-project.forEach(item => {
-  item.addEventListener('mouseover', () => {
-      removeClass();
-
-      item.classList.add('selected');
-  })
-
-  removeClass = () => {
-    project.forEach(item => {
-        item.addEventListener('mouseout', () => {
-            item.classList.remove('selected');
-          })
-    })
-  }
-})
-
 // Procreate
 
 const back = document.querySelector('#back');
 const next = document.querySelector('#next');
+const number = document.querySelector('#number');
+const outof = document.querySelector('#outof');
 
-const photos = ['img/projects/livingroom.jpg', 'img/projects/military.jpg', 'img/projects/kitchen.jpg', 'img/projects/black_bathroom.jpg', 'img/projects/blue_sofa.jpg', 'img/projects/livingroom_bed.jpg', 'img/projects/manscave.jpg', 'img/projects/fireplace.jpg', 'img/projects/toilet.jpg'];
+const photos = ['img/projects/livingroom.jpg',
+                'img/projects/military.jpg', 
+                'img/projects/kitchen.jpg', 
+                'img/projects/black_bathroom.jpg', 
+                'img/projects/blue_sofa.jpg', 
+                'img/projects/livingroom_bed.jpg', 
+                'img/projects/manscave.jpg', 
+                'img/projects/fireplace.jpg', 
+                'img/projects/toilet.jpg'];
 
 let i = 0;
+let count = 1;
+let total = photos.length
+number.innerText = count;
+outof.innerText = total;
 
 next.addEventListener('click', () => {
     i++;
+    count++;
 
-    if (i > photos.length - 1) {
+    if (i > total - 1) {
         i = 0;
+        count = 1;
     }
 
     document.querySelector('#procreate-img').src = photos[i];
+
+    number.innerText = count;
+    outof.innerText = total;
 })
 
 back.addEventListener('click', () => {
     i--;
+    count--;
 
     if (i < 0) {
-        i = photos.length - 1;
+        i = total - 1;
+        count = total;
     }
 
     document.querySelector('#procreate-img').src = photos[i];
+
+    number.innerText = count;
+    outof.innerText = total;
 })
+
+// Projects
+
+const project = document.querySelectorAll('.project');
+
+project.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        removeClass();
+
+        item.classList.add('selected');
+    })
+
+    removeClass = () => {
+        project.forEach(item => {
+            item.addEventListener('mouseout', () => {
+                item.classList.remove('selected');
+            })
+        })
+    }
+})
+
+function responsive(a, b) {
+    if (a.matches || b.matches) {
+        project.forEach(item => {
+            item.addEventListener('click', () => {
+                item.classList.toggle('selected');
+            })
+        })
+    }
+}
+  
+let a = window.matchMedia("(max-width: 820px")
+responsive(a);
+a.addEventListener(responsive);
+
+let b = window.matchMedia("(max-width: 500px")
+responsive(b);
+b.addEventListener(responsive);
